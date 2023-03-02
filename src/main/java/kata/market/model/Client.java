33cb2dd30fd510927670601bf14f6b2cbb87187b
ttayech <devigeniance23@gmail.com>
2023-03-02 01:16:46 +0100

    Add body to addToCart method for client class

--- src/main/java/kata/market/model/Client.java
+++ src/main/java/kata/market/model/Client.java
@@ -10,7 +10,12 @@ class Client {
     private LinkedHashMap cart = new LinkedHashMap<Product, Float>();
 
     void addToCart(Product product, Float numberToBuy) {
+        if (cart.containsKey(product)) {
 
+            cart.put(product, numberToBuy + (Float) cart.get(product));
+        } else {
+            cart.put(product, numberToBuy);
+        }
     }
 
 }

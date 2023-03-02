92e06bf165c26b514858867c81b4170b1e72ad76
ttayech <devigeniance23@gmail.com>
2023-03-02 01:33:10 +0100

    Adding reduction to supermarket

---
+++ src/main/java/kata/market/model/Supermarket.java
@@ -0,0 +1,14 @@
+package kata.market.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+class Supermarket {
+
+    void addReduction(Product product, int numberToBuy, float reduction) {
+    }
+
+}
---
+++ src/test/java/kata/market/model/SupermarketTest.java
@@ -0,0 +1,43 @@
+package kata.market.model;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SupermarketTest {
+
+    private Supermarket superMarket;
+
+
+    @Test
+    public void item_should_correctly_be_updated_when_reduction_created() {
+        //given
+        superMarket = new Supermarket();
+        Product product = new Product(false, null, "washing gel", 50);
+
+        //when
+        superMarket.addReduction(product, 2, 0.5f);
+
+        //then
+        assertThat(product.getReductionValueByNumber()._1(), is(2));
+        assertThat(product.getReductionValueByNumber()._2(), is(0.5f));
+    }
+
+    @Test
+    public void item_reduction_should_correctly_be_replaced_when_new_reduction_created() {
+        //given
+        superMarket = new Supermarket();
+        Product product = new Product(false, null, "washing gel", 50);
+        superMarket.addReduction(product, 10, 0.9f);
+
+        //when
+        superMarket.addReduction(product, 2, 0.5f);
+
+        //then
+        assertThat(product.getReductionValueByNumber()._1(), is(2));
+        assertThat(product.getReductionValueByNumber()._2(), is(0.5f));
+    }
+
+
+}

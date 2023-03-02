6b5f7ff0d7dff048613bcae23f5b0624d4ac09cd
ttayech <devigeniance23@gmail.com>
2023-03-02 02:01:54 +0100

    Implement the calcul of the total price

--- src/main/java/kata/market/model/Supermarket.java
+++ src/main/java/kata/market/model/Supermarket.java
@@ -12,10 +12,35 @@ import java.util.*;
 @AllArgsConstructor
 class Supermarket {
 
+    private Optional<Integer> itemPromotion(Product product) {
+        return Optional.ofNullable(product.getReductionValueByNumber())
+                .filter(reductionList -> Objects.nonNull(reductionList._1))
+                .filter(reductionList -> Objects.nonNull(reductionList._2))
+                .map(reductionList -> reductionList._1);
+    }
+
     float calculateTotalAmount(Client customer) {
         LinkedHashMap<Product, Float> input = customer.getCart();
         float total = 0f;
+
+        total = processDefaultPricing(input, total);
+        total = processPromotionPricing(input, total);
+
         return total;
+    }
+
+    private float processPromotionPricing(LinkedHashMap<Product, Float> inputMap, float total) {
+        return inputMap.entrySet().stream()
+                .filter(item -> itemPromotion(item.getKey()).isPresent())
+                .map(item -> new PromotionPricing().calculateTotalAmount(item.getKey(), item.getValue()))
+                .reduce(total, (f1, f2) -> f1 + f2);
+    }
+
+    private float processDefaultPricing(LinkedHashMap<Product, Float> inputMap, float total) {
+        return inputMap.entrySet().stream()
+                .filter(item -> !itemPromotion(item.getKey()).isPresent())
+                .map(item -> new DefaultPricing().calculateTotalAmount(item.getKey(), item.getValue()))
+                .reduce(total, (f1, f2) -> f1 + f2);
     }
 
     void addReduction(Product product, int numberToBuy, float reduction) {

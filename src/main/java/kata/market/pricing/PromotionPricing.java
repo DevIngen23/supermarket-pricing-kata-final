1642bd09fbb16b78fe67daf65c75b4c84cec1d84
ttayech <devigeniance23@gmail.com>
2023-03-02 01:50:16 +0100

    implement the calcul of promotion pricing

--- src/main/java/kata/market/pricing/PromotionPricing.java
+++ src/main/java/kata/market/pricing/PromotionPricing.java
@@ -8,6 +8,12 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class PromotionPricing extends Pricing {
     public float calculateTotalAmount(Product product, Float numberBought) {
-        return 0f;
+        int numberForReduction = product.getReductionValueByNumber()._1();
+        int timesReductionApplied = (int) (numberBought / numberForReduction);
+        float numberOfFullPriceGoods = numberBought % numberForReduction;
+        float reductionValue = product.getReductionValueByNumber()._2();
+        float reducedPrice = timesReductionApplied * product.getUnitaryPrice() * numberForReduction * reductionValue;
+        float unreducedPrice = numberOfFullPriceGoods * product.getUnitaryPrice();
+        return reducedPrice + unreducedPrice;
     }
 }
